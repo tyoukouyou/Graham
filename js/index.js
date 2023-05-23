98d8adbfc6be1f989ec885cb678d23dc0f7b5ebc
tyoukouyou <3300653033@qq.com>
2023-05-23 18:01:08 +0900

    2023/05/23 evening

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-window.onload = function () {
   if (window.innerWidth > 1000) {
     //navbar color change
     var links = document.querySelectorAll('#headerContainer>header>.header_nav>.item_ul>li');
     links.forEach(function (link) {
       link.addEventListener('click', function () {
+        console.log('aaaaaaa');
         var previousActiveLi = document.querySelector('#headerContainer>header>.header_nav>.item_ul>li.active');
         if (previousActiveLi) {
           previousActiveLi.classList.remove('active');
@@ -14,6 +14,7 @@ window.onload = function () {
       });
     });
 
+
     //mouse scroll
     let mouse_btn = document.querySelectorAll("#mouse_btn");
     mouse_btn[0].addEventListener("click", scrollToNextSection);
@@ -22,13 +23,9 @@ window.onload = function () {
       window.scrollBy(0, window.innerHeight);
     }
   }
-  if (window.innerWidth < 1000) {
   
 
-  }
 
-
-}
 if (window.innerWidth < 1000) {
   function showNavBar() {
     var headerMenu = document.getElementById('header_menu');
